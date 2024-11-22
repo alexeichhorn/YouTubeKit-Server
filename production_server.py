c365855a238da99627ec8ac8d8497b982e4c6132
Alexander Eichhorn <a.eichhorn@losjet.com>
2024-11-22 15:03:11 +0100

    added info log level in production

--- production_server.py
+++ production_server.py
@@ -1,5 +1,7 @@
+import logging
 from aiohttp import web
 from server import app
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     web.run_app(app, host="0.0.0.0", port=8080)
