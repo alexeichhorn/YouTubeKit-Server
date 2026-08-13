99cf66315cd2e95150293a91f457dd6b6dc25f14
Alexander Eichhorn <a.eichhorn@losjet.com>
2026-08-13 11:29:24 +0200

    update youtubei.js

--- src/youtube/models/internal.ts
+++ src/youtube/models/internal.ts
@@ -1,11 +1,9 @@
 // Using the same string literal type as youtubei.js internally uses
 export type AvailableInnertubeClient =
    | 'IOS'
-   | 'IOS_DOWNGRADED'
    | 'WEB'
    | 'MWEB'
    | 'ANDROID'
-   | 'ANDROID_SDKLESS'
    | 'ANDROID_VR'
    | 'YTMUSIC'
    | 'YTMUSIC_ANDROID'
